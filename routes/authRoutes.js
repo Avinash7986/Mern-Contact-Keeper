8411790f2412244c0ae281c972e3915e70c0e322
Avinash7986 <avinashsingh7986@gmail.com>
2020-08-18 11:34:48 +0530

    Added Authentication

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,4 +1,10 @@
 const express = require('express');
+const { check, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const config = require('config');
+const jwt = require('jsonwebtoken');
+const auth = require('../middleware/auth');
+const User = require('../models/User');
 
 const router = express.Router();
 
@@ -6,16 +12,69 @@ const router = express.Router();
 // @desc    Login a user
 // @access  Public
 
-router.post('/', (req, res) => {
-  res.send('Login a user');
-});
+router.post(
+  '/',
+  [
+    check('email', 'Please enter your email').isEmail(),
+    check('password', 'Please enter your password').exists(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array()[0].msg });
+    }
+
+    const { email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res.status(400).json({ msg: 'Invalid Credentials' });
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch) {
+        return res.status(400).json({ msg: 'Invalid Credentials' });
+      }
+
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtsecret'),
+        {
+          expiresIn: 360000,
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json(token);
+        }
+      );
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
 
 // @route   GET api/auth
 // @desc    Get Logged in user
 // @access  Private
 
-router.get('/', (req, res) => {
-  res.send('User Details');
+router.get('/', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.send(user);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server Error');
+  }
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const authRoutes = require('./routes/authRoutes');
 const contactRoutes = require('./routes/contactRoutes');
 const userRoutes = require('./routes/userRoutes');
 
 const app = express();
+
+// Connect To Databse
+connectDB();
 
 app.use(express.json());
 
